e44b046963f4ec7ca653c84b7d831e7cf3b50529
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-07-30 17:30:31 +0300

    editing the db models

--- E-commerce-server/app/Models/Product.php
+++ E-commerce-server/app/Models/Product.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'description', 'category', 'image_url',
+    ];
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function cart()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
